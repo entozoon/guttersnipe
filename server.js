4b942c9a179255204245261f5e256f3c60a01d46
Mickey <entozoon@gmail.com>
2023-02-08 22:47:44 +0000

    Early package dev..

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
 "use strict";
 exports.__esModule = true;
-var node_path_1 = require("node:path");
-var node_fs_1 = require("node:fs");
-var express_1 = require("express");
-var livereload_1 = require("livereload");
-var connect_livereload_1 = require("connect-livereload");
+var path = require("path");
+var fs = require("fs");
+var express = require("express");
+var livereload = require("livereload");
+var connectLivereload = require("connect-livereload");
 var port = 6969;
 //
-var liveReloadServer = livereload_1["default"].createServer();
-liveReloadServer.watch(node_path_1["default"].join(__dirname, "cms"));
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "cms"));
 liveReloadServer.server.once("connection", function () {
     setTimeout(function () {
         liveReloadServer.refresh("/");
     }, 100);
 });
-var app = (0, express_1["default"])();
-app.use((0, connect_livereload_1["default"])());
+var app = express();
+app.use(connectLivereload());
 //
 app.listen(port, function () { return console.log("Listening on port ".concat(port)); });
 //
 // Typical routes
 app.get("/", function (req, res) {
-    res.sendFile(node_path_1["default"].join(__dirname, "../cms/index.html"));
+    res.sendFile(path.join(__dirname, "../cms/index.html"));
 });
 app.get("/posts/*", function (req, res) {
-    res.sendFile(node_path_1["default"].join(__dirname, "../cms/post.html"));
+    res.sendFile(path.join(__dirname, "../cms/post.html"));
 });
 //
 // API routes
-app.use(express_1["default"].static(node_path_1["default"].join(__dirname, "dist")));
-var router = express_1["default"].Router();
-var postsPath = node_path_1["default"].join(__dirname, "../public/posts.json");
+app.use(express.static(path.join(__dirname, "dist")));
+var router = express.Router();
+var postsPath = path.join(__dirname, "./data.json");
 var getPosts = function (req, res) {
-    var posts = JSON.parse(node_fs_1["default"].readFileSync(postsPath));
+    console.log(":: ~ getPosts postsPath", postsPath);
+    var posts = JSON.parse(fs.readFileSync(postsPath).toString());
+    console.log(":: ~ getPosts posts", posts);
     return res.json(posts);
 };
-var getSchema = function (req, res) {
-    var schema = JSON.parse(node_fs_1["default"].readFileSync(node_path_1["default"].join(__dirname, "../public/schema.json")));
-    return res.json(schema);
-};
-var getPost = function (req, res) {
-    var posts = JSON.parse(node_fs_1["default"].readFileSync(postsPath));
-    return res.json(posts[parseInt(req.params.i)]);
-};
+// const getSchema = (req: any, res: any) => {
+//   const schema = JSON.parse(
+//     fs.readFileSync(path.join(__dirname, "../public/schema.json"))
+//   );
+//   return res.json(schema);
+// };
+// const getPost = (req: any, res: any) => {
+//   const posts = JSON.parse(fs.readFileSync(postsPath));
+//   return res.json(posts[parseInt(req.params.i)]);
+// };
 var createPost = function (req, res) {
     console.log("::createPost", req, res);
 };
@@ -53,9 +57,9 @@ var deletePost = function (req, res) {
     console.log("::deletePost", req, res);
 };
 app.use("/api", router);
-router.get("/schema", getSchema);
+// router.get("/schema", getSchema);
 router.get("/posts", getPosts);
-router.get("/posts/:i", getPost);
+// router.get("/posts/:i", getPost);
 router.post("/posts", createPost);
 router.put("/posts/:id", updatePost);
 // router.delete("/posts/:id", deletePost);
